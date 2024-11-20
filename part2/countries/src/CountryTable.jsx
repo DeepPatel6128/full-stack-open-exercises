aad16bf0babc72e601345d855d10ee4393b3e162
Deep <deepatel6128@gmail.com>
2024-11-20 18:23:06 -0500

    Part 2 completed

--- part2/countries/src/CountryTable.jsx
+++ part2/countries/src/CountryTable.jsx
@@ -1,19 +1,37 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import countryService from './services/countryService'
+
 
 /* eslint-disable react/prop-types */
 export default function CountryTable({ countries, filterValue, filterCountries }) {
   const [selectedCountry, setSelectedCountry] = useState({});
-
+  const [weather, setWeather] = useState({})
+  const getWeather = (country)=>{
+    countryService.getGeoCode(country?.capital).then((res)=>{
+        countryService.getWeather(res.data[0].lon, res.data[0].lat).then((res)=>{
+          const temp = (res.data.main.temp - 273.15).toPrecision(2)
+          const wind = res.data.wind.speed;
+          const imgURL = `https://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`
+          setWeather({temperature: temp, wind: wind, imgURL: imgURL})
+        }).catch((e)=>{
+          console.log(e.message)
+        })
+    }).catch((e)=>{
+      console.log(e.message)
+    })
+  }
    useEffect(() => {
     if (countries.length === 1) {
       setSelectedCountry(countries[0]);
+      getWeather(countries[0]);
     }
     else setSelectedCountry({});
   }, [countries]);
-  
+
   const handleCountryClick = (country) => {
     setSelectedCountry(country);
+    getWeather(country);
   };
 
   return (
@@ -49,7 +67,12 @@ export default function CountryTable({ countries, filterValue, filterCountries }
               ))
               ) : <li>No languages found</li>}
             </ul>
+            
             <img src={selectedCountry?.flags?.png || 'No image found'}/>
+            <h2>Weather in {selectedCountry?.capital}</h2>
+            <p>temperature: {weather.temperature} celcius</p>
+            <img src={weather.imgURL}/>
+            <p>wind: {weather.wind} m/s</p>
           </div>}
     </div>
   );
--- part2/countries/src/services/countryService.js
+++ part2/countries/src/services/countryService.js
@@ -1,7 +1,23 @@
 import axios from "axios";
 const baseURL = "https://studies.cs.helsinki.fi/restcountries/api/all";
+
 const getAllCountries = () => {
   return axios.get(baseURL);
 };
 
-export default { getAllCountries };
+const getGeoCode = (city) => {
+  return axios.get(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${
+      import.meta.env.VITE_SOME_KEY
+    }`
+  );
+};
+
+const getWeather = (lon, lat) => {
+  return axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${
+      import.meta.env.VITE_SOME_KEY
+    }`
+  );
+};
+export default { getAllCountries, getGeoCode, getWeather };
